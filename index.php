6f052b90190a7a076424abe4d3ccd22cd3f7fbfe
Anatoly Glinin <glinin@me.com>
2017-01-31 20:54:54 +1000

    add: menu sidebar

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <!-- JS -->
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <!-- Body wrapper -->
+        <div id="body-wrapper">
+            <!-- Header -->
+            <div id="header">
+                <div class="row">
+                    <div class="col-md-3 col-xs-3">
+                        <div id="logo">                           
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-xs-5">
+                        <div class="header_orgname">Бетонный завод ООО "Бетон-ДВ"</div>
+                    </div>
+                    <div class="col-md-3 col-xs-3">
+                        <div class="header_phone pull-right">
+                            <span class="phone">+7 (4217) 50-00-00<br /></span>
+                            <span class="phone">+7 (999) 123-45-67<br /></span>
+                            <span class="org_address">г. Комсомольск-на-Амуре<br />ул. Магистральная, 17/1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Header / End -->
+            
+        <div class="row">  
+            <!-- Menu -->
+            <div class="col-md-3 col-xs-3">
+                <ul class="menu">
+                    <li class="menu_header">Продукция</li>
+                    <li>
+                        <ul> 
+                            <li class="menu_img_01"><a href="#">ЖБИ</a></li>
+                            <li class="menu_img_02"><a href="#">Бетон товарный</a></li>
+                            <li class="menu_img_03"><a href="#">Растворы</a></li>
+                            <li class="menu_img_04"><a href="#">Строительные блоки</a></li>
+                            <li class="menu_img_05"><a href="#">Тротуарная плитка, брусчатка</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu_header">Услуги</li>
+                    <li>
+                        <ul> 
+                            <li class="menu_img_06"><a href="#">Доставка</a></li>
+                            <li class="menu_img_07"><a href="#">Бетононасос</a></li>
+                            <li class="menu_img_08"><a href="#">Весы автомобильные</a></li>
+                            <li class="menu_img_09"><a href="#">Погрузка / выгрузка вагонов</a></li>
+                            <li class="menu_img_10"><a href="#">Хранение груза</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- Menu / End -->
+       
+            <!-- Navigation -->
+            <div class="col-md-9 col-xs-9">
+                <!--<div id="navigation">-->
+                    <nav class="navbar navbar-default">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Главная</a></li>
+                            <li><a href="about.php">О компании</a></li>
+                            <li><a href="price.php">Прайс-лист</a></li>
+                            <li><a href="contacts.php">Контакты</a></li>
+                        </ul>
+                    </nav>
+                <!--</div>-->
+            </div>
+            <!-- Navigation / End -->         
+        </div>
+                
+            <!-- Content -->
+            
+            <!-- Content / End -->
+            
+            <!-- Footer -->
+            <div class="navbar-fixed-bottom row-fluid">
+                <div class="container">
+                    
+                </div>
+            </div>
+            <!-- Footer / End -->
+        </div>
+        <!-- Body wrapper / End -->
+    </body>
+</html>
+
