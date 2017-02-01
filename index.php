1261c006cae162c93bc65ec8a43bd8125d766f4b
Anatoly Glinin <glinin@me.com>
2017-02-01 19:38:22 +1000

    redesign

--- index.php
+++ index.php
@@ -5,92 +5,182 @@
         <!-- CSS -->
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/slick.css" />
+        <link rel="stylesheet" href="css/slick-theme.css" />
+
         <!-- JS -->
-        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/jquery-2.2.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <script src="js/slick.min.js"></script>
         <title></title>
     </head>
     <body>
-        <!-- Body wrapper -->
-        <div id="body-wrapper">
+
+        <div class="container">
             <!-- Header -->
             <div id="header">
                 <div class="row">
-                    <div class="col-md-3 col-xs-3">
-                        <div id="logo">                           
-                        </div>
+                    <div class="col-xs-5">
+                        <div class="logo-first"></div>
+                        <div class="logo-second"></div>
+                        <div class="logo-third"></div>
                     </div>
-                    <div class="col-md-5 col-xs-5">
-                        <div class="header_orgname">Бетонный завод ООО "Бетон-ДВ"</div>
-                    </div>
-                    <div class="col-md-3 col-xs-3">
-                        <div class="header_phone pull-right">
-                            <span class="phone">+7 (4217) 50-00-00<br /></span>
-                            <span class="phone">+7 (999) 123-45-67<br /></span>
-                            <span class="org_address">г. Комсомольск-на-Амуре<br />ул. Магистральная, 17/1</span>
-                        </div>
+                    <div class="header_phone pull-right">
+                        <span class="callHeader">ПОЗВОНИТЬ НАМ</span><br/>
+                        <span class="phonePrefix">+7 (4217) </span><span class="phoneNumber">51-22-66</span><br/>
+                        <span class="phoneNumber">+7 962 286 9777</span>
                     </div>
                 </div>
             </div>
             <!-- Header / End -->
-            
-        <div class="row">  
-            <!-- Menu -->
-            <div class="col-md-3 col-xs-3">
-                <ul class="menu">
-                    <li class="menu_header">Продукция</li>
-                    <li>
-                        <ul> 
-                            <li class="menu_img_01"><a href="#">ЖБИ</a></li>
-                            <li class="menu_img_02"><a href="#">Бетон товарный</a></li>
-                            <li class="menu_img_03"><a href="#">Растворы</a></li>
-                            <li class="menu_img_04"><a href="#">Строительные блоки</a></li>
-                            <li class="menu_img_05"><a href="#">Тротуарная плитка, брусчатка</a></li>
+            <!-- Body wrapper -->  
+            <div id="body-wrapper">
+                <div class="row">
+
+                    <!-- Content -->
+                    <div class="col-md-9 col-xs-9 col-md-push-3 content">
+                        <!-- Navigation -->
+                        <div id="navigation">
+                            <ul id="nav">
+                                <li class="active"><a href="index.php">Главная</a></li>
+                                <li><a href="about.php">О компании</a></li>
+                                <li><a href="about.php">Галерея</a></li>
+                                <li><a href="about.php">Вакансии</a></li>
+                                <li><a href="contacts.php">Контакты</a></li>
+                            </ul>
+                        </div>
+                        <!-- Navigation / End -->    
+
+                        <div class="slider">
+                            <div>
+                                <img src="images/slider/slider_01.jpg">
+                            </div>
+                            <div>
+                                <img src="images/slider/slider_02.jpg">
+                            </div>
+                            <div>
+                                <img src="images/slider/slider_03.jpg">
+                            </div>
+                            <div>
+                                <img src="images/slider/slider_04.jpg">
+                            </div>
+                            <div>
+                                <img src="images/slider/slider_05.jpg">
+                            </div>
+                        </div>
+                        
+                   
+                            <div style="height: 175px; background-color: #FDBB05; padding-top: 2px; text-align: center; color: #01212E; font-size: 10px; font-weight: bold;">
+                            <div class="labelProduction">Наше производство</div>
+                            <div>
+                                ПОСТОЯННОЕ УВЕЛИЧЕНИЕ МОЩНОСТЕЙ ПРОИЗВОДСТВА ПОЗВОЛЯЕТ НАМ ЗАНИМАТЬ ВЫСОКИЕ ПОЗИЦИИ <BR/> В КОНКУРЕНТНОЙ СРЕДЕ РЕГИОНА И БЫТЬ В ЧИСЛЕ ЛИДЕРОВ ГОРОДА
+                            </div>
+                            <div style="margin: 0 auto">
+                                
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left; margin: 20px 20px 20px 54px; border-radius: 10px;"></div>
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left;margin: 20px;border-radius: 10px;"></div>
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left; margin: 20px;border-radius: 10px; "></div>
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left; margin: 20px;border-radius: 10px;"></div>
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left; margin: 20px;border-radius: 10px;"></div>
+                            <div style="width: 90px; height: 70px; background-color: #FEFBF2; float: left; margin: 20px;border-radius: 10px;"></div>
+                            </div>
+                            </div>
+                       
+                        
+
+                    </div>
+
+                    <!-- Content / End -->    
+
+                    <!-- Menu -->
+                    <div class="col-md-3 col-xs-3 col-md-pull-9 leftside_menu">
+                        <ul class="menu">
+                            <li class="menu_header">Продукция</li>
+                            <li>
+                                <ul> 
+                                    <li class="menu_img_01"><a href="#">ЖБИ</a></li>
+                                    <li class="menu_img_02"><a href="#">Бетон товарный</a></li>
+                                    <li class="menu_img_03"><a href="#">Растворы</a></li>
+                                    <li class="menu_img_04"><a href="#">Строительные блоки</a></li>
+                                    <li class="menu_img_05"><a href="#">Тротуарная плитка, брусчатка</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu_header">Услуги</li>
+                            <li>
+                                <ul> 
+                                    <li class="menu_img_06"><a href="#">Доставка</a></li>
+                                    <li class="menu_img_07"><a href="#">Бетононасос</a></li>
+                                    <li class="menu_img_08"><a href="#">Весы автомобильные</a></li>
+                                    <li class="menu_img_09"><a href="#">Погрузка / выгрузка вагонов</a></li>
+                                    <li class="menu_img_10"><a href="#">Хранение груза</a></li>
+                                </ul>
+                            </li>
                         </ul>
-                    </li>
-                    <li class="menu_header">Услуги</li>
-                    <li>
-                        <ul> 
-                            <li class="menu_img_06"><a href="#">Доставка</a></li>
-                            <li class="menu_img_07"><a href="#">Бетононасос</a></li>
-                            <li class="menu_img_08"><a href="#">Весы автомобильные</a></li>
-                            <li class="menu_img_09"><a href="#">Погрузка / выгрузка вагонов</a></li>
-                            <li class="menu_img_10"><a href="#">Хранение груза</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- Menu / End -->
-       
-            <!-- Navigation -->
-            <div class="col-md-9 col-xs-9">
-                <!--<div id="navigation">-->
-                    <nav class="navbar navbar-default">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="index.php">Главная</a></li>
-                            <li><a href="about.php">О компании</a></li>
-                            <li><a href="price.php">Прайс-лист</a></li>
-                            <li><a href="contacts.php">Контакты</a></li>
-                        </ul>
-                    </nav>
-                <!--</div>-->
-            </div>
-            <!-- Navigation / End -->         
-        </div>
+                    </div>
+                    <!-- Menu / End -->
+
+
+
+
+
+                </div>
                 
-            <!-- Content -->
-            <!-- test1 -->
-            <!-- Content / End -->
-            
-            <!-- Footer -->
-            <div class="navbar-fixed-bottom row-fluid">
-                <div class="container">
-                    
+                <div class="row">
+                    <div class="col-md-12">
+                    <div class="labelProduction">НАШИ ПАРТНЁРЫ</div>
+                                        
+                                                    <div style="height: 160px; background-color: #01212E; padding-top: 2px;">
+                            
+                            </div>
+                    </div>
                 </div>
             </div>
-            <!-- Footer / End -->
+            <!-- Body wrapper / End -->
         </div>
-        <!-- Body wrapper / End -->
+
+        <!-- Footer -->
+        <footer class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="footer-inner">
+                        <div class="col-xs-7">
+                            <div class="logo-footer-first"></div>
+                            <div class="logo-footer-second"></div>
+                            <div class="logo-footer-third"></div>
+                        </div>
+                        <div class="col-xs-3 footer_address">
+                            ООО «АКВАМАРИН» <br/>
+                            681005, г. Комсомольск-на-Амуре <br/>
+                            ул. Чернышевского, 5/2
+                        </div>
+                        <div class="col-xs-2 footer_contacts">
+                            тел.: +7(4217) 51-22-66 <br/>
+                            моб.: +7 962 286 9777
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Footer / End -->
+
+        <script type="text/javascript">
+            $(document).on('ready', function () {
+                $(".slider").slick({
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    dots: true,
+                    infinite: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    fade: true,
+                    cssEase: 'linear',
+                        prevArrow: false,
+    nextArrow: false
+                });
+            });
+        </script>
+
     </body>
 </html>
 
